874c760f7d5b2abec376189e583863001f576bbc
mnish <mnish@LAPTOP-9HAUE24K>
2025-09-29 12:34:13 +0700

    1st push

---
+++ src/main/java/com/mymart/posexample/POSExample.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mymart.posexample;
+
+/**
+ *
+ * @author mnish
+ */
+public class POSExample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
